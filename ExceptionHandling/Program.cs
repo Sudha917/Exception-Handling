9ed2ea71ebb717cc1a09cda0ab0bd80d49ab11d3
sudha917 <sudha.avinash917.com>
2018-09-05 15:29:52 +0530

    Exception Handling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        public static void Display(string s)
+        {
+            if (s == null)
+            {
+                throw new Exception("Argument Can not be null");
+            }
+        }
+        
+    }
+    class Master:Program
+    { 
+        static void Main(string[] args)
+        {
+            string s = null;
+            int x=100, y=0;
+
+            try
+            {
+                Display(s);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                Console.WriteLine(x/ y);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division of {0} by zero.", x);
+            }
+        
+            Console.ReadLine();
+        }
+    }
+}
